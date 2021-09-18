7884c014b6148120acd4bbb130c401af661db23d
Shahid Haider <shaider@bccrc.ca>
2021-09-17 17:55:59 -0700

    Sorting using optional data_organization file

--- main.py
+++ main.py
@@ -11,22 +11,27 @@ import re
 import cv2
 from reliability.Other_functions import crosshairs
 
+import pandas as pd
+from collections import deque
+
+
 @click.group()
 def cli():
     pass
 
 refPt = []
-@cli.command('makeBarcodeWithBead')
-@click.argument('img647_dir',type=click.Path(exists=True))
-@click.argument('img750_dir',type=click.Path(exists=True))
-@click.argument('bead_dir',type=click.Path(exists=True))
-@click.option('--n_ref',default=0,type=int)
-@click.option('--pattern',default=None,type=str)
-@click.option('--circle_size',default=15,type=int)
-@click.option('--thresh',default=0.995,type=float)
-@click.option('--window_size',default=100,type=int)
-@click.option('--img_scale',default=10.0,type=float)
-def makeBarcodeWithBead(img647_dir:str,img750_dir:str,bead_dir:str,n_ref:int,pattern:str,circle_size:int,thresh:float,window_size:int,img_scale:float):
+# @cli.command('makeBarcodeWithBead')
+# @click.argument('img647_dir',type=click.Path(exists=True))
+# @click.argument('img750_dir',type=click.Path(exists=True))
+# @click.argument('bead_dir',type=click.Path(exists=True))
+# @click.option('--n_ref',default=0,type=int)
+# @click.option('--pattern',default=None,type=str)
+# @click.option('--circle_size',default=15,type=int)
+# @click.option('--thresh',default=0.995,type=float)
+# @click.option('--window_size',default=100,type=int)
+# @click.option('--img_scale',default=10.0,type=float)
+# @click.option('--data_org',default=None,type=click.Path(exists=True))
+def makeBarcodeWithBead(img647_dir:str,img750_dir:str,bead_dir:str,n_ref:int,pattern:str,circle_size:int,thresh:float,window_size:int,img_scale:float,data_org:str):
 
     
 
@@ -41,7 +46,7 @@ def makeBarcodeWithBead(img647_dir:str,img750_dir:str,bead_dir:str,n_ref:int,pat
     
     file_list_647.sort(key=lambda f: int(re.sub('\D', '', f)))
     file_list_750.sort(key=lambda f: int(re.sub('\D', '', f)))
-
+    
     bead_dir_pattern = os.path.join(bead_dir,pattern)
     bead_file_list = glob(bead_dir_pattern)
     bead_file_list.sort(key=lambda f: int(re.sub('\D', '', f)))
@@ -53,16 +58,36 @@ def makeBarcodeWithBead(img647_dir:str,img750_dir:str,bead_dir:str,n_ref:int,pat
         bead_imgs[idx]=img
     #load images
     imgs = dict()
-    file_list = file_list_647+file_list_750
+    
+    if data_org is not None:
+        file_list=list()
+        df = pd.read_excel(data_org)
+        frame_num = df['frame']
+        
+        q_647 = deque(file_list_647)
+        q_750 = deque(file_list_750)
+        for row in frame_num.iteritems():
+            
+            if row[1] == 2:
+                if q_750:
+                    file_list.append(q_750.popleft())
+            elif row[1] ==1:
+                if q_647:
+                    file_list.append(q_647.popleft())
+            
+    else:
+        file_list = file_list_647+file_list_750
+    
+    
     for idx,fn in enumerate(file_list):
         
         _img = skio.imread(fn)
         
-        imgs[idx]=equalize_adapthist(_img, clip_limit=0.03)
+        imgs[idx]=_img#equalize_adapthist(_img, clip_limit=0.03)
 
     
     for idx in range(1,len(bead_imgs)):
-        shift,_,_ = registration.phase_cross_correlation(bead_imgs[n_ref],bead_imgs[idx])
+        shift,_,_ = registration.phase_cross_correlation(bead_imgs[n_ref]/2**9,bead_imgs[idx]/2**9,upsample_factor=10)
         
         imgs[idx] = ndi.shift(imgs[idx],shift)
 
@@ -79,7 +104,6 @@ def makeBarcodeWithBead(img647_dir:str,img750_dir:str,bead_dir:str,n_ref:int,pat
     crosshairs()
     plt.show()
     while True:
-  
         if len(refPt)<2:
             if not plt.fignum_exists(num=1):
                 fig,ax = plt.subplots(num=1)
@@ -130,6 +154,7 @@ def recordClickLoc_mpl(event):
     global refPt
     refPt=[int(event.xdata),int(event.ydata)]
 
+
 def centreCrop(img,centrePt,window_size):
     h=window_size//2
 
@@ -162,12 +187,13 @@ def calcSpotThresh(img:np.ndarray,thresh_percent:float = 0.9)->float:
     #find the threshpercentile. i.e. sort the list, and then find the thresh_percent*len(list)
 
 if __name__=='__main__':
-    cli()
+    #cli()
 
-    # img647_dir = '/Volumes/shahidsWORK/647nm, Raw'
+    img647_dir = '/Volumes/shahidsWORK/647nm, Raw'
 
-    # img750_dir = '/Volumes/shahidsWORK/750nm, Raw'
+    img750_dir = '/Volumes/shahidsWORK/750nm, Raw'
 
-    # beads_dir = '/Volumes/shahidsWORK/561nm, Raw'
+    beads_dir = '/Volumes/shahidsWORK/561nm, Raw'
+    data_org = '/Volumes/shahidsWORK/data_organization.xlsx'
 
-    # makeBarcodeWithBead(img647_dir,img750_dir,beads_dir,n_ref=0,pattern="merFISH_*_002_01.TIFF",circle_size=9,thresh=0.995,window_size=50,img_scale=1)
+    makeBarcodeWithBead(img647_dir,img750_dir,beads_dir,n_ref=0,pattern="merFISH_*_003_01.TIFF",circle_size=9,thresh=0.99,window_size=50,img_scale=1,data_org=data_org)
